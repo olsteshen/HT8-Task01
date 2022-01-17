78ac5155f24b3801391232f1e76dacfaf6dee1f2
stesho01 <Listo4ek>
2022-01-17 17:14:38 +0200

    Task 01 - 1st commit

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -3,7 +3,6 @@ package com.example.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Main {
